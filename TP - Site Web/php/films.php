5d1ec7a9e6962b529d094a30d246c986987dd720
Rigaud Marc-Antoine <125704673+RMarcAntoine@users.noreply.github.com>
2023-03-07 00:53:03 +0100

    Add files via upload

---
+++ TP - Site Web/php/films.php
@@ -0,0 +1,372 @@
+<?php
+
+include('films.php');
+
+$films = array(
+    array(
+        'titre' => 'Film 1',
+        'realisateur' => 'Réalisateur 1',
+        'annee' => 2020,
+        'duree' => 120,
+        'genre' => 'Action'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+    array(
+        'titre' => 'Film 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2019,
+        'duree' => 90,
+        'genre' => 'Comédie'
+    ),
+
+
+);
+
+
+foreach ($films as $film) {
+    echo '<div class="film">';
+    echo '<h3>' . $film['titre'] . '</h3>';
+    echo '<p>Réalisateur : ' . $film['realisateur'] . '</p>';
+    echo '<p>Année : ' . $film['annee'] . '</p>';
+    echo '<p>Durée : ' . $film['duree'] . ' minutes</p>';
+    echo '<p>Genre : ' . $film['genre'] . '</p>';
+    echo '</div>';
+}
+?>
+
+<
---
+++ TP - Site Web/php/series.php
@@ -0,0 +1,369 @@
+<?php
+
+include('series.php');
+
+$series = array(
+    array(
+        'titre' => 'Série 1',
+        'realisateur' => 'Réalisateur 1',
+        'annee' => 2019,
+        'saisons' => 3,
+        'episodes' => 30
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 3',
+        'realisateur' => 'Réalisateur 3',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 4',
+        'realisateur' => 'Réalisateur 4',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 5',
+        'realisateur' => 'Réalisateur 5',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+    array(
+        'titre' => 'Série 2',
+        'realisateur' => 'Réalisateur 2',
+        'annee' => 2020,
+        'saisons' => 2,
+        'episodes' => 20
+    ),
+
+
+);
+
+
+foreach ($series as $serie) {
+    echo '<div class="serie">';
+    echo '<h3>' . $serie['titre'] . '</h3>';
+    echo '<p>Réalisateur : ' . $serie['realisateur'] . '</p>';
+    echo '<p>Année : ' . $serie['annee'] . '</p>';
+    echo '<p>Saisons : ' . $serie['saisons'] . '</p>';
+    echo '<p>Episodes : ' . $serie['episodes'] . '</p>';
+    echo '</div>';
+}
